d2d3685d2f73594b6b3f0f7294132b6cf604bf7d
Jonathan Freeman <freethejazz@gmail.com>
2018-01-25 17:46:08 -0600

    Migrates progress styles to aphrodite

--- src/components/ProgressSeeker.js
+++ src/components/ProgressSeeker.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import './progress.css';
+import { StyleSheet, css } from 'aphrodite';
 
 const ProgressSeeker = (props) => {
   return (
-    <div className="progress-area">
-      <div className="progress-wrapper">
+    <div className={css(styles.progressArea)}>
+      <div className={css(styles.progressShape)}>
         <div className="progress-loaded" />
-        <div className="progress-played" style={{width: `${props.value * 100}%`}}/>
+        <div className={css(styles.progressShape, styles.progressPlayed)} style={{width: `${props.value * 100}%`}}/>
         <div className="progress-current" />
       </div>
     </div>
   );
 }
+
+const styles = StyleSheet.create({
+  progressArea: {
+    padding: 10,
+  },
+  progressShape: {
+    height: 6,
+    borderRadius: 6,
+    backgroundColor: '#333'
+  },
+  progressPlayed: {
+    backgroundColor: '#666'
+  }
+})
 
 export default ProgressSeeker;
