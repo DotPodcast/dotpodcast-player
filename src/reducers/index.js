a62bdc5e52648949e048cc996b83d219f252bb0b
Mark Steadman <mark@bloomsbury.digital>
2018-01-30 17:23:06 +0000

    Adds podcastDetail reducer to reducer list

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,11 +3,13 @@ import { routerReducer } from 'react-router-redux';
 
 import playerReducer from './player';
 import searchReducer from './search';
+import podcastDetailReducer from './podcast-detail';
 
 const dotpodcastPlayer = combineReducers({
   player: playerReducer,
   router: routerReducer,
   search: searchReducer,
+  podcastDetail: podcastDetailReducer
 });
 
 export default dotpodcastPlayer;
