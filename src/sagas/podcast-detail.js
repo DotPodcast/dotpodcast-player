72a04a926118c7a5679150565075bf88ab717223
Mark Steadman <mark@bloomsbury.digital>
2018-01-30 17:29:30 +0000

    Causes the podcast-detail saga to throw an error if no podcasts are found matching the query

--- src/sagas/podcast-detail.js
+++ src/sagas/podcast-detail.js
@@ -5,9 +5,13 @@ import { actions } from '../reducers/podcast-detail';
 export function* getPodcastDetails(action) {
   try {
     const result = yield call(getPodcastDetailsFromEs, action.slug);
+
+    if(!result.hits.hits.length) {
+      throw new Error('No podcasts found.')
+    }
+
     yield put(actions.detailRetrieved(result));
   } catch(e) {
     yield put(actions.detailError(e));
   }
 }
-
