8741d1def231eed7bbc94c7e52bdfa91d8427032
Jonathan Freeman <freethejazz@gmail.com>
2018-02-02 15:51:49 -0600

    Nests layout within routes for login/callback flexibility

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router';
 import { Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
+import Layout from '../containers/Layout';
 
 /* Route Components */
 import Home from '../containers/Home';
@@ -9,15 +10,23 @@ import PodcastDetail from '../containers/PodcastDetail';
 import Callback from '../containers/Callback';
 import LoginSplash from '../containers/LoginSplash';
 
+const InLayoutRouter = (props) => {
+  return (
+    <Layout>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/:slug" component={PodcastDetail} />
+    </Layout>
+  )
+}
+
 const AppRouter = (props) => {
   return (
     <ConnectedRouter history={props.history}>
       <div>
         <Switch>
-          <Route exact path="/" component={Home} />
           <Route exact path="/login" component={LoginSplash} />
           <Route exact path="/callback" component={Callback} />
-          <Route exact path="/:slug" component={PodcastDetail} />
+          <Route path="/" component={InLayoutRouter} />
         </Switch>
       </div>
     </ConnectedRouter>
