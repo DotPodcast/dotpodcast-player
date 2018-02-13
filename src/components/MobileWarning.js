355cc60bf24f7e7caeb0c6129baf3ef8dfc585e0
EricG <eric.gay@gmail.com>
2018-02-13 17:54:18 -0500

    Fix mobile logic and render bug

--- src/components/MobileWarning.js
+++ src/components/MobileWarning.js
@@ -4,12 +4,14 @@ import { isMobile } from 'react-device-detect';
 
 class MobileWarning extends Component {
   render() {
-    if (!isMobile)
+    if (isMobile)
       return(
-        <Alert bsStyle='warning' style={{margin:"20px"}}>
+        <Alert bsStyle='warning' style={{margin:"5px"}}>
           DotPodcast requires the BlockStack browser to securely store your user data from prying eyes in the cloud, which is not available on mobile browsers just yet. <a href="http://docs.dotpodcast.co">Read More</a>
         </Alert>
-    );
+      );
+    else
+      return null;
   }
 }
 
