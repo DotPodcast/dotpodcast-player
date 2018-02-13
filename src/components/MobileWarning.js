55f4c03f8c55df23a87bc9b8b6e9b61976fbc01d
EricG <eric.gay@gmail.com>
2018-02-13 17:47:57 -0500

    Display a warning message for mobile users on the login page.

---
+++ src/components/MobileWarning.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Alert } from 'react-bootstrap';
+import { isMobile } from 'react-device-detect';
+
+class MobileWarning extends Component {
+  render() {
+    if (!isMobile)
+      return(
+        <Alert bsStyle='warning' style={{margin:"20px"}}>
+          DotPodcast requires the BlockStack browser to securely store your user data from prying eyes in the cloud, which is not available on mobile browsers just yet. <a href="http://docs.dotpodcast.co">Read More</a>
+        </Alert>
+    );
+  }
+}
+
+export default MobileWarning;
