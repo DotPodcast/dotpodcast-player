516b569538ae649ec75d7ccde2233b84b97cc018
EricG <eric.gay@gmail.com>
2018-02-19 16:30:50 -0500

    grab users publicKey

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,4 +1,5 @@
 import makeTypes from '../utils/makeTypes';
+import { decodeToken } from 'jsontokens'
 
 export const types = makeTypes([
   'USER_LOGIN_CALLBACK',
@@ -29,6 +30,7 @@ export const actions = {
 const defaultState = {
   loadingUser: false,
   appPrivateKey: '',
+  publicKey: '',
   authResponseToken: '',
   hubUrl: '',
   coreSessionToken: null,
@@ -44,13 +46,11 @@ const reducer = (state = defaultState, action) => {
         loadingUser: true,
       };
     case types.USER_DETAILS_LOADED:
-      console.log("USER");
-      console.log(action.data);
-
       return {
         ...state,
         loadingUser: false,
         ...action.data,
+        publicKey: decodeToken(action.data.authResponseToken).payload.public_keys[0]
       };
     case types.USER_DETAILS_FAILED:
       return {
