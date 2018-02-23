44594d21ec8391f7de320bf346e1c86095e38843
Beaker <eric.gay@gmail.com>
2018-02-23 17:51:03 -0500

    hack the issue with no username

--- src/components/MiniPlayButton.js
+++ src/components/MiniPlayButton.js
@@ -3,7 +3,7 @@ import GlyphButton from './GlyphButton';
 
 const miniPlayButton = function(props) {
   const dispatch = () => {
-    props.action(props.username, props.podcast, props.episode);
+    props.action(props.podcast, props.episode);
   }
 
   return (
