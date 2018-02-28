9e74ba4e7ded04462da907f08cabcbd04c1dea53
EricG <eric.gay@gmail.com>
2018-02-28 16:21:15 +0100

    fix spelling

--- src/utils/alerts.js
+++ src/utils/alerts.js
@@ -2,7 +2,7 @@ export const anonymousPlayAlert = () => {
   const key = "viewed_anonymous_play_alert";
 
   if (!localStorage.getItem(key)) {
-    alert("Thanks for trying us out. For full functionality, such as subcribing and supporting podcasters, you need the Blockstack Browser installed. Enjoy the episode!");
+    alert("Thanks for trying us out. For full functionality, such as subscribing and supporting podcasters, you need the Blockstack Browser installed. Enjoy the episode!");
     localStorage.setItem(key, true)
   }
 }
