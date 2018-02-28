a63b9a4af0a86fa8a2317ff580d91501e83fdf9b
EricG <eric.gay@gmail.com>
2018-03-01 00:21:28 +0100

    Cleaner nav and about us page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Row, Col } from 'react-bootstrap';
 import { StyleSheet, css } from 'aphrodite';
 
 class ContactUs extends Component {
   render() {
     return (
-      <div className={css(styles.content)}>
-        <h2>Get In Touch</h2>
-        <div>
-          Have feedback or questions? Please <a href="mailto:info@dotpodcast.co">send us a message!</a>
-        </div>
-      </div>
+      <Grid fluid>
+        <Row>
+          <Col xs={12}>
+            <h3>About DotPodcast</h3>
+            Search, subscribe, and pay podcasters without being tracked or censored.
+         </Col>
+        </Row>
+
+        <Row>
+          <Col xs={12}>
+            <h3>Podcast Hosting</h3>
+            DPX is our open source self-hosted platform, allowing you to quickly upload new episodes, or migrate an existing podcast while supporting all the new features of the DotPodcast protocol. DPX provides a legacy RSS feed, so you can still serve traditional podcast clients and directories. If you’re familiar with Docker and have previously hosted your own websites, you can deploy your own DPX site by following the instructions on GitHub.
+
+            If you’d like to take advantage of the DotPodcast protocol but would prefer someone else look after the technical side, <Link to="/contact">email us.</Link>       
+          </Col>
+        </Row>
+
+        <Row>
+          <Col xs={12}>
+            <h3>Get In Touch</h3>
+            Have feedback or questions? Please <a href="mailto:info@dotpodcast.co">send us a message.</a>
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  content: {
-    margin: 10
-  }
 });
 
 export default ContactUs;
